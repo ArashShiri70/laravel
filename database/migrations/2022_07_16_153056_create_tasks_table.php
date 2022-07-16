dea2363167b697abf29aee130327c63d989799c2
ArashShiri70 <arashshir70@gmail.com>
2022-07-16 23:21:56 +0430

    prepare migration

---
+++ database/migrations/2022_07_16_153056_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('owner_id');
+            $table->integer('created_by');
+            $table->integer('deleted_by');
+            $table->string('task_name');
+            $table->string('status');
+            $table->date('add_date');
+            $table->date('change_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
